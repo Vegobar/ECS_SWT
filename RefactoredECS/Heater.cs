4e1a30a4f05cf65e038ddd1c3a9410b6aa2bf005
Lena Todnem <lena.todnem@gmail.com>
2020-02-12 17:25:28 +0100

    Added them interfacezz

---
+++ RefactoredECS/Heater.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactoredECS
+{
+    public class Heater : IHeater
+    {
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+
+        public void TurnOff()
+        {
+            System.Console.WriteLine("Heater is off");
+        }
+
+        public void TurnOn()
+        {
+            System.Console.WriteLine("Heater is on");
+        }
+    }
+}
---
+++ RefactoredECS/TempSensor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactoredECS
+{
+    internal class TempSensor : ISensor
+    {
+        private Random gen = new Random();
+        public int GetTemp()
+        {
+            return gen.Next(-5, 45);
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+}
