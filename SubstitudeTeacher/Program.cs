2e23666909016e991bcab31dfa429d89ff0ecb02
Mikkel Schmidt Andersen <Mikkel220897@hotmail.com>
2020-02-19 14:58:55 +0100

    added test case heater. but not implemented

--- SubstitudeTeacher/Program.cs
+++ SubstitudeTeacher/Program.cs
@@ -4,17 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using RefactoredECS;
 
 namespace SubstitudeTeacher
 {
     [TestFixture]
     class Sub_tester
     {
-        
+
         [SetUp]
         public void Setup()
         {
             _
+        }
+
+        [Test]
+        public void test_Heater()
+        {
+
         }
     }
 }
