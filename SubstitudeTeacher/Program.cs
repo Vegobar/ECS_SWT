15fad24531ea64bd7c1b9b5d85be8977c2fe5d4d
Mikkel Schmidt Andersen <Mikkel220897@hotmail.com>
2020-02-19 15:20:43 +0100

    Did setup file

--- SubstitudeTeacher/Program.cs
+++ SubstitudeTeacher/Program.cs
@@ -6,27 +6,40 @@ using System.Threading.Tasks;
 using NSubstitute;
 using NUnit.Framework;
 using RefactoredECS;
+using ECS = RefactoredECS.ECS;
 
 namespace SubstitudeTeacher
 {
     [TestFixture]
     class Sub_tester
     {
-        private IHeater heater;
-        private ISensor sensor;
+        private IHeater _heater;
+        private ISensor _sensor;
+        private IWindow _window;
+        private RefactoredECS.ECS _uut;
 
         [SetUp]
         public void Setup()
         {
-            heater = Substitute.For<IHeater>();
-            sensor = Substitute.For<ISensor>();
+            _heater = Substitute.For<IHeater>();
+            _sensor = Substitute.For<ISensor>();
+            _window = Substitute.For<IWindow>();
 
+            _uut = new RefactoredECS.ECS(_sensor,_heater,_window,30,50);
+            
         }
 
         [Test]
         public void test_Heater()
         {
+            _sensor.GetTemperature().Returns(_uut.GetThreshold())
 
+        }
+
+        [Test]
+        public void test_Window()
+        {
+            _sensor.G
         }
     }
 }
